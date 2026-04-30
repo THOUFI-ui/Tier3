67839b0ef3fb4ede0953897333fc5a4cb4dc0e5d
THOUFI-ui <thethoufy@gmail.com>
2026-04-30 11:27:35 +0400

    Update database URI in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     SECRET_KEY = "super-secret-key"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:rootpassword@mysql-service:3306/mydb"
+        "mysql+pymysql://root:rootpassword@mysql:3306/mydb"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
