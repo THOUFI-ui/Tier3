a89ccc3edcdbc475dc02fb067ba5e8f49bac0548
THOUFI-ui <thethoufy@gmail.com>
2026-05-12 12:33:58 +0400

    Change message column length from 255 to 100

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from .extensions import db
+from app import db
 
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(255), nullable=False)
+    message = db.Column(db.String(100))
